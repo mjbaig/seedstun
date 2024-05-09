599a650065a329d032eef63f146a9e8193ac3b0b
maz <7367135+mjbaig@users.noreply.github.com>
2024-05-08 23:16:32 -0700

    wrote a token file

---
+++ src/token.zig
@@ -0,0 +1,24 @@
+pub const ILLEGAL = "ILLEGAL";
+
+pub const EOF = "EOF";
+
+// Identifiers and Literal
+pub const IDENT = "IDENT";
+pub const INT = "INT";
+
+// Operators
+pub const ASSIGN = "=";
+pub const PLUS = "+";
+
+// Delimiters
+pub const COMMA = ",";
+pub const SEMICOLON = ";";
+
+pub const LPAREN = "(";
+pub const RPAREN = ")";
+pub const LBRACE = "{";
+pub const RBRACE = "}";
+
+// Keywords
+pub const FUNCTION = "FUNCTION";
+pub const LET = "LET";
